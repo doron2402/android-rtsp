dcd4d941f0b10858fd9c56fbda436484fd617314
Doron Segal <doron2402@gmail.com>
2024-06-29 01:14:26 +0300

    init

---
+++ app/src/main/java/com/mkrdeveloper/videoplayercompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.mkrdeveloper.videoplayercompose
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.mkrdeveloper.videoplayercompose.ui.theme.VideoPlayerComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            VideoPlayerComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    /* val videoUri =
+                         Uri.parse("android.resource://com.mkrdeveloper.videoplayercompose/raw/sample")*/
+//                     val videoUrl = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+//                    val videoUrl = Uri.parse("rtsp://82.166.176.26:8554/bla")
+//                    val videoUrl = "rtsp://82.166.176.26:8554/bla")
+                    Column {
+                        /*YouTubePlayer(
+                            youtubeVideoId = "kShAS6aafOU",
+                            lifecycleOwner = LocalLifecycleOwner.current
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        VideoPlayer(videoUri =videoUri)*/
+                        VideoPlayerExo(videoUrl ="rtsp://fake.kerberos.io/stream")
+//                        VideoPlayer(videoUri = Uri.parse("http://82.166.176.26:8554/bla"))
+
+                    }
+
+                }
+            }
+        }
+    }
+}
+
